f57e17ffeb603222c1f1d26d16f4e1928ec6e389
Bas Brands <bas@sonsbeekmedia.nl>
2025-09-16 13:52:36 +0200

    bump version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'filter_teacherprofile';
 $plugin->release      = '1.0';
-$plugin->version      = 2025062600;
+$plugin->version      = 2025101600;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 405];
 $plugin->maturity     = MATURITY_STABLE;
